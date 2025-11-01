1fc00a89ff59c3b80f35d627b726bbd58ff631c9
Dmitry Avdoshenkov <avdoshenkov@yandex.ru>
2025-11-01 21:00:05 +0100

    feat: update Fixel font files

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,17 +3,17 @@ import localFont from 'next/font/local';
 export const fixel = localFont({
   src: [
     {
-      path: '../../public/fonts/FixelText-Regular.woff2',
+      path: '../../public/fonts/FixelDisplay-Regular.woff2',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '../../public/fonts/FixelText-Medium.woff2',
+      path: '../../public/fonts/FixelDisplay-Medium.woff2',
       weight: '500',
       style: 'normal'
     },
     {
-      path: '../../public/fonts/FixelText-SemiBold.woff2',
+      path: '../../public/fonts/FixelDisplay-SemiBold.woff2',
       weight: '600',
       style: 'normal'
     }
