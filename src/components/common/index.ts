6d20a5506ea02d689471a20dc4ec730bcbb4b79a
Dmitry Avdoshenkov <avdoshenkov@yandex.ru>
2025-11-04 20:18:46 +0100

    chore: export CopyButton and PageHeader components from common index

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { Button, type TButtonProps } from './Button';
 export { TextField } from './TextField';
+export { CopyButton, type TCopyButtonProps } from './CopyButton';
+export { PageHeader, type TPageHeaderProps } from './PageHeader';
 export * from './icons';
