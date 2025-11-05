c80cf1f3571b9a4c7dcdf530edb9da4a1c2f476f
Dmitry Avdoshenkov <avdoshenkov@yandex.ru>
2025-11-05 16:30:50 +0100

    feat: update Button in Header for consistent UI

--- src/components/header/Header/Header.tsx
+++ src/components/header/Header/Header.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Header.module.css';
 import { HeaderProgress } from '../HeaderProgress';
-import { HomeIcon } from '@/components/common';
+import { HomeIcon, Button } from '@/components/common';
 import {
   selectLettersCount,
   selectGoalCount,
   selectIsGoalReached,
   useCoverLetterStore
 } from '@/store/coverLetters';
+import { useRouter } from 'next/navigation';
 
 export const Header = () => {
   const current = useCoverLetterStore(selectLettersCount);
   const goal = useCoverLetterStore(selectGoalCount);
   const reached = useCoverLetterStore(selectIsGoalReached);
+  const router = useRouter();
+
+  const handleHome = () => {
+    // might be link but for now use button to have a consistent style
+    router.push('/');
+  };
 
   return (
     <header className={styles.header}>
@@ -23,9 +29,7 @@ export const Header = () => {
         <Image src="/img/logo.svg" loading="eager" alt="Alt + Shift Logo" width={179} height={48} />
       </div>
       <HeaderProgress current={current} goal={goal} reached={reached} />
-      <Link className={styles.homeLink} href="/">
-        <HomeIcon />
-      </Link>
+      <Button variant="outline" icon={<HomeIcon />} onClick={handleHome} />
     </header>
   );
 };
